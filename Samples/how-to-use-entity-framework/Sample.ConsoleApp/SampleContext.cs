71b8c883ddfbe75db1bd8755dd0e5f37922006d9
kuro <kuro_q@live.com>
2017-12-24 20:48:14 +0800

    add readme.md

---
+++ Samples/how-to-use-entity-framework/Sample.ConsoleApp/SampleContext.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Sample.ConsoleApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class SampleContext
+    {
+    }
+}
